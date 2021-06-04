aa19d974b471337b6a42ea65a597919e881433d2
Kasia <kasia.bondarava@gmail.com>
2021-06-04 16:32:02 -0400

    Delete functionality

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", getPosts);
 // Listen for add post
 document.querySelector(".post-submit").addEventListener("click", submitPosts);
 
+// Listen for delete
+document.querySelector("#posts").addEventListener("click", deletePost);
+
 // Get Posts
 function getPosts() {
   http.get("http://localhost:3000/posts")
@@ -42,6 +45,29 @@ function submitPosts() {
         console.log(err);
         ui.showAlert("Error while posting", "alert alert-danger");
       });
+  } 
+}
+
+// Delete post
+
+function deletePost(e) {
+
+  if(e.target.parentElement.classList.contains("delete")) {
+    const id = e.target.parentElement.dataset.id;
+
+    if(confirm("Are you sure?")) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+      .then(res => {
+        ui.showAlert("Post is successfully deleted", "alert alert-success");
+        getPosts();
+      })
+      .catch(err => {
+        console.log(err);
+        ui.showAlert("Error while deleting", "alert alert-danger")
+      })
+    }
+
+    
   }
-  
+  e.preventDefault();
 }
